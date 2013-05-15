8f02147dac5660cfa8941b7682f79134caddff34
jacobcr <jcr@tid.es>
2013-05-15 09:35:25 +0200

    branch working with human curl

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from distutils.core import setup
 
 setup(
     name='python-gcm',
-    version='0.1.3',
-    packages=['gcm'],
+    version='0.1.4',
+    packages=['gcm', 'human_curl'],
     license=open('LICENSE').read(),
     author='Minh Nam Ngo',
     author_email='nam@namis.me',
     url='http://blog.namis.me/python-gcm/',
     description='Python client for Google Cloud Messaging for Android (GCM)',
     long_description=open('README.md').read(),
-    keywords='android gcm push notification google cloud messaging',
-    tests_require = ['mock'],
+    keywords='android gcm push notification google cloud messaging'
 )
